7a941b30530873d0d86c2875a714b4ec58d3af28
Sruthi Sami <sheisruthisami@gmail.com>
2026-04-20 21:50:56 +0530

    os memory

--- exercise-9/modules/os.js
+++ exercise-9/modules/os.js
@@ -1,11 +1,14 @@
 const os = require("os");
 
 function getSystemInfo() {
+    const totalMemMB = (os.totalmem() / (1024 * 1024)).toFixed(2);
+    const freeMemMB = (os.freemem() / (1024 * 1024)).toFixed(2);
+
     return `
 OS Platform: ${os.platform()}
 Architecture: ${os.arch()}
-Total Memory: ${os.totalmem()}
-Free Memory: ${os.freemem()}
+Total Memory: ${totalMemMB} MB
+Free Memory: ${freeMemMB} MB
 CPU Cores: ${os.cpus().length}
 Hostname: ${os.hostname()}
 `;
